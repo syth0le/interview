20520c3d329d09eb2db632158c57494f346e28cd
syth0le <chdan565@gmail.com>
2022-09-25 11:30:41 +0300

    Added reverse for linked list

---
+++ Companies/Algorithms Section/6_Linked_LIst/1_reverse.py
@@ -0,0 +1,24 @@
+# Развернуть связный список за линейное время
+
+class Node:
+
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+def reverse(head: Node) -> Node:
+    tail = None
+    while head:
+        print(head.data, end=' -> ') if head.next else print(head.data)
+        head.next, tail, head = tail, head, head.next
+    return tail
+
+
+if __name__ == '__main__':
+    head = Node(1, Node(2, Node(3, Node(4))))
+    result = reverse(head)
+    while result:
+        print(result.data, end=' -> ') if result.next else print(result.data)
+        result = result.next
+
