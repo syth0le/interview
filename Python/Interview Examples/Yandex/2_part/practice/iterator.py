2c16442bf38d8671fe899414e8cbfefab376b441
syth0le <chdan565@gmail.com>
2022-10-07 23:47:38 +0300

    added info about iterators

--- Python/Interview Examples/Yandex/2_part/practice/iterator.py
+++ Python/Interview Examples/Yandex/2_part/practice/iterator.py
@@ -1,14 +1,19 @@
 class Iterator:
-    def __init__(self, array, cursor):
+    def __init__(self, array, cursor=0):
         self._array = array
-        self._cursor = cursor
+        self._cursor = cursor - 1
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self._cursor < len(self._array):
+        if self._cursor < len(self._array) - 1:
             self._cursor += 1
-            return 1
+            return self._array[self._cursor]
         else:
             raise StopIteration
+
+
+it = Iterator([1, 2, 3, 4, 5, 1, 5, 3, 5, 3, 5], 0)
+for item in it:
+    print(item, end=' ')
